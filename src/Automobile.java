73c887771e379b013d794ebfcfbdfcb772e7c3af
MiguelBasilio <lokoa@DESKTOP-F4BVPJT>
2016-04-27 19:48:09 -0700

    auto setting number of doors, name, color, Cylinders...

---
+++ src/Automobile.java
@@ -0,0 +1,48 @@
+
+public class Automobile {
+   int doors;
+   int cylinders;
+   String color;
+   String name;
+   int transmission;
+   
+   public Automobile () {
+	   doors = 4;
+	   cylinders = 6;
+	   name = "Car";
+	   
+   }
+   public int setNumberofDoors (int d){
+	   doors = d;
+	   return doors;
+	      }
+   public int setNumberofCylinders (int c){
+	   cylinders = c;
+			   return cylinders;
+   }
+   public String setColor (String Cl){
+	   color = Cl;
+	   return color;
+   }
+   public String setName(String n){
+	   name= n;
+	   return name;
+   }
+   public int setTransmission(int t){
+	   transmission = t;
+			   return transmission;
+	   
+   }
+
+   public String info(){
+	   String nameA = "Number of doors: " + doors+ "Number of cylinders: " + cylinders; 
+       nameA = nameA +"Transmission: " + transmission; 
+       nameA= nameA +"Color: " + color; 
+       nameA = nameA +"Name: " + name; 
+        return nameA; 
+   }
+	   
+   
+	
+
+}
---
+++ src/Cars.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Cars {
+
+	public static void main(String[] args) {
+	        Automobile yellow = new Automobile(); 
+	        yellow.setNumberofDoors(4); 
+	        yellow.setColor("Yellow"); 
+	        yellow.setName("Cabbie"); 
+	        System.out.println(yellow.info()); 
+	         
+	        Automobile red = new Automobile(); 
+	         red.setNumberofDoors(2); 
+	         red.setColor("Red"); 
+	         red.setName("Sporty"); 
+	         System.out.println(red.info());
+	         
+	          
+	         Automobile green = new Automobile(); 
+	         green.setNumberofDoors(5); 
+	         green.setColor("Green"); 
+	         green.setName("Vanny"); 
+	         System.out.println(green.info());
+	         
+	     } 
+
+	}
+
+
